295be98413806ac9dfc6fbd42c21803acc9798fb
AlekseyGaman <4117988@gmail.com>
2023-03-07 00:11:32 +0200

    try to fix node

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const app = require("./app");
-const mongoose = require("mongoose");
+// const app = require("./app");
+// const mongoose = require("mongoose");
 
-const DB_HOST =
-  "mongodb+srv://Aleksey:UerTKLg0KAm21nlS@cluster0.csjbdx7.mongodb.net/db-contacts?retryWrites=true&w=majority";
+// const DB_HOST =
+//   "mongodb+srv://Aleksey:UerTKLg0KAm21nlS@cluster0.csjbdx7.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
-mongoose.set("strictQuery", true);
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(3000);
-  })
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });
+// mongoose.set("strictQuery", true);
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => {
+//     app.listen(3000);
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//     process.exit(1);
+//   });
