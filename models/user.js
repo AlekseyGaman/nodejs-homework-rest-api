aa1e44d1ef0e990c4f1219522766168d7b1c8fa1
AlekseyGaman <4117988@gmail.com>
2023-03-13 21:33:38 +0200

    delete email regex

--- models/user.js
+++ models/user.js
@@ -3,9 +3,6 @@ const Joi = require("joi");
 
 const { handleMongooseError } = require("../helpers");
 
-// eslint-disable-next-line no-useless-escape
-const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
 const userSchema = new Schema(
   {
     password: {
@@ -16,7 +13,6 @@ const userSchema = new Schema(
     email: {
       type: String,
       required: [true, "Email is required"],
-      match: emailRegexp,
       unique: true,
     },
     subscription: {
@@ -40,12 +36,12 @@ userSchema.post("save", handleMongooseError);
 
 const registerSchema = Joi.object({
   password: Joi.string().min(6).required(),
-  email: Joi.string().pattern(emailRegexp).required(),
+  email: Joi.string().required(),
 });
 
 const loginSchema = Joi.object({
   password: Joi.string().min(6).required(),
-  email: Joi.string().pattern(emailRegexp).required(),
+  email: Joi.string().required(),
 });
 
 const User = model("contact", userSchema);
